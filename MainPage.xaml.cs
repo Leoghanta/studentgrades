27c4bcb1576548f5ab51f155aba22ef399893891
Thomas Wilson <t.wilson@dundeeandangus.ac.uk>
2023-11-02 21:30:39 +0000

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace studentgrades
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<student> students = new ObservableCollection<student>();
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            AddStudents();
+        }
+
+        /// <summary>
+        /// Add a list of students as test data
+        /// </summary>
+        private void AddStudents()
+        {
+            students.Add(new student("Fred Jones", 72));
+            students.Add(new student("Daphne Blake", 68));
+            students.Add(new student("Velma Dinkley", 89));
+            students.Add(new student("Shaggy Rogers", 55));
+            students.Add(new student("Scoobert Doo", 59));
+        }
+
+        /// <summary>
+        /// Display a message to the user.
+        /// </summary>
+        /// <param name="message">Message being sent</param>
+        /// <param name="title">A nice title</param>
+        private async void DisplayMessage(string message, string title)
+        {
+            MessageDialog msg = new MessageDialog(message, title);
+            await msg.ShowAsync();
+        }
+
+        /// <summary>
+        /// Event Handler when user clicks Add button
+        /// </summary>
+        /// <param name="sender">Button being clicked</param>
+        /// <param name="e">Any parameters (there is none!)</param>
+		private void Add_Click(object sender, RoutedEventArgs e)
+		{
+            // Nothing in the student name? Send them an error!
+            if (string.IsNullOrEmpty(studentNameTextBox.Text))
+            {
+                DisplayMessage("Student name can not be empty!", "Error");
+                return;
+            }
+
+            // Nothing in the marks? send them an error!
+            if (string.IsNullOrEmpty (marksTextBox.Text))
+            {
+                DisplayMessage("Marks can not be empty!", "Error");
+                
+            }
+
+            // New Student may throw an exception. int.Parse may also throw an exception
+            // encase in try..catch clause
+            try
+            {
+                //try and add the students.
+                students.Add(new student(studentNameTextBox.Text, int.Parse(marksTextBox.Text)));
+                studentNameTextBox.Text = string.Empty; // clear the text box
+                marksTextBox.Text = string.Empty;       // clear the text box
+            } 
+            catch (Exception ex)
+            {
+                DisplayMessage(ex.Message, "Error");
+            }
+
+		}
+	}
+}
---
+++ student.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentgrades
+{
+	internal class student
+	{
+		private int _marks;
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Add marks to the student record.  Check the mark is in range or throw exception.
+		/// </summary>
+		public int Marks
+		{
+			get { return _marks; }
+			private set
+			{
+				if (_marks != value)
+				{
+					if (value < 0 || value > 100)
+					{
+						throw new Exception("Studnet Marks out of range!");
+					}
+					else
+					{
+						_marks = value;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Do not set grade, only return it depending on mark
+		/// </summary>
+		public string Grade
+		{
+			get
+			{
+				if (Marks >= 90)
+				{
+					return "A";
+				}
+				else if (Marks >= 80)
+				{
+					return "B";
+				}
+				else if (Marks >= 70)
+				{
+					return "C";
+				}
+				else if (Marks >= 60)
+				{
+					return "D";
+				}
+				else
+				{
+					return "F";
+				}
+			}
+		}
+
+		/// <summary>
+		/// Constructor for student
+		/// </summary>
+		/// <param name="name">Name as a string</param>
+		/// <param name="mark">Mark as an integer</param>
+		public student(string name, int mark)
+		{
+			Name = name;
+			Marks = mark;
+		}
+	}
+}
